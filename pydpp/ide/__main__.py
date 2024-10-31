38a11746148d0bb68cde7ac7fe3b99e68d4d798d
Arthur-N42 <arthur.nz.ju@gmail.com>
2024-10-31 17:03:24 +0100

    instauring text highlighting

--- pydpp/ide/__main__.py
+++ pydpp/ide/__main__.py
@@ -5,6 +5,11 @@ import tkinter.messagebox
 import customtkinter
 from tkinter import filedialog
 import os
+from pydpp.compiler import ProblemSet, ProblemSeverity
+from pydpp.compiler.parser import parse
+from pydpp.compiler.tokenizer import tokenize, TokenKind
+
+
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme(os.path.join(os.path.dirname(__file__), 'Metadata/style.json'))  # Themes: "blue" (standard), "green", "dark-blue"
 
@@ -63,11 +68,11 @@ class App(customtkinter.CTk):
         self.tabview.grid_columnconfigure(0, weight=1)
         self.tabview.add("Menu").grid_columnconfigure(0, weight=1)
 
-        self.textboxes = {} #liste de références de textbox pour les tab
+        self.textboxes = {} # List of textboxes for every file + Menu
 
-        text=customtkinter.CTkTextbox(self.tabview.tab("Menu")) #création de la textbox du tab Menu
-        text.insert("1.0","Ici les règles et commandes") #Ne marche pas pour l'instant
-        self.textboxes["Menu"] = text #rajout de la textbox à la liste de textbox
+        menu = customtkinter.CTkTextbox(self.tabview.tab("Menu")) # Creating textbox of Menu tab
+        menu.insert("1.0","Ici les règles et commandes") #Ne marche pas pour l'instant
+        self.textboxes["Menu"] = menu #rajout de la textbox à la liste de textbox
 
 
 
@@ -139,8 +144,67 @@ class App(customtkinter.CTk):
             self.tabview.add(name).grid_columnconfigure(0, weight=1)
             self.tabview.tab(name).grid_rowconfigure(0, weight=1)
             showtext=customtkinter.CTkTextbox(self.tabview.tab(name))  
-            showtext.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")        
+            showtext.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+            self.init_highlighting(showtext)
+            showtext.bind("<<Modified>>", lambda e: self.update_highlighting(showtext))
             self.textboxes[name] = showtext #ajout de la référence de la textbox dans la liste des références
+    
+    def init_highlighting(self, txt: customtkinter.CTkTextbox):
+        # Configure all syntax highlighting tags
+        txt.tag_config("kw", foreground="#268bd2")
+        txt.tag_config("str", foreground="#cb4b16")
+        txt.tag_config("num", foreground="#859900")
+        txt.tag_config("err", underline=True, underlinefg="red")
+
+        def err_enter(er):
+            print("ERROR ENTER: cursor is at", txt.index("current"))
+            # Then use that coordinate to find where the error is and show a tooltip... somehow
+
+        def err_exit(er):
+            print(f"ERROR EXIT: cursor stepped away ({txt.index("current")} now)")
+
+        # Bind some functions to run when the cursor enters or leaves an error in the text.
+        # We can use that to show tooltips!
+        txt.tag_bind("err", "<Enter>", err_enter)
+        txt.tag_bind("err", "<Leave>", err_exit)
+    
+    def update_highlighting(self, txt: customtkinter.CTkTextbox):
+        # Converts FileCoordinates into tkinter coordinates
+        def fc_to_idx(fc):
+            return f"{fc.line}.{fc.column-1}"
+
+        # Clear all existing highlighting
+        txt.tag_remove("kw", "1.0", "end")
+        txt.tag_remove("str", "1.0", "end")
+        txt.tag_remove("num", "1.0", "end")
+        txt.tag_remove("err", "1.0", "end")
+
+        # Get the entire text of the textbox
+        t = txt.get("1.0", "end")
+
+        # Run the tokenizer (for primary syntax highlighting) and the parser (for error recognition)
+        ps = ProblemSet() # Errors will be there
+        tkn_list = tokenize(t, ps)
+        tree = parse(tkn_list, ps) # not used yet, costly!
+
+        # Highlight every portion of the text that matches with a token
+        for t in tkn_list:
+            if t.kind.name.startswith("KW") or t.kind == TokenKind.LITERAL_BOOL:
+                # Keyword
+                txt.tag_add("kw", fc_to_idx(t.pos.start), fc_to_idx(t.pos.end))
+            elif t.kind == TokenKind.LITERAL_STRING:
+                # String
+                txt.tag_add("str", fc_to_idx(t.pos.start), fc_to_idx(t.pos.end))
+            elif t.kind == TokenKind.LITERAL_NUM:
+                # Number
+                txt.tag_add("num", fc_to_idx(t.pos.start), fc_to_idx(t.pos.end))
+
+        # Highlight every error (not warnings for now)
+        for e in ps.grouped[ProblemSeverity.ERROR]:
+            txt.tag_add("err", fc_to_idx(e.pos.start), fc_to_idx(e.pos.end))
+
+        # Set modified to False so the event triggers again (it's dumb but that's how it works)
+        txt.edit_modified(False)
 
 if __name__ == "__main__":
     app = App()
