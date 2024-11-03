fe8e5c2a4b890d40e7e646cdcbb316ef41b9fa35
DonatienLinossier <donatien.linossier@gmail.com>
2024-11-03 23:22:56 +0100

    reorg : Implement DLL for SDL encapsulation
    Refactor build process to utilize a DLL for SDL encapsulation, allowing only the generated 'tmp.c' to be recompiled instead of the entire source. This change aims to speed up compilation times.

---
+++ libs/sdlEncapsulation/cmake/sdlencapsulation-config.cmake
@@ -0,0 +1,1 @@
+set(sdlEncapsulation TRUE)
--- pydpp/compiler/CTranslater/CTranslater.py
+++ pydpp/compiler/CTranslater/CTranslater.py
@@ -39,17 +39,30 @@ from ._Function import Function
 from .Variable import VarCall
 
 
+'''All functions and variables (varDict) related to the main function have been deleted as the main function is now a simple function'''
+
 class CTranslater:
     def __init__(self, filename):
+
+        #TODO: make verif about the file
         self.filename = filename
         self.file = open(self.filename, "w")
         self.header()
 
+
+
+
+        #self.main is the entry point of the created program
         #TODO: Add the possibility to run with arguments
         #TODO: add the possibility to have optionals arguments
         self.main = Function("Main", [])
 
+
+        '''
+        #Self.varDict contains the variables of the main function.
         self.varDict = {}
+        '''
+        #self.instr store all the core functions of the compilater and the functions created by the user.
         self.instr = {"createVar": self.createVar,
                       "getVar": self.getVar,
                       "addToVar": self.addToVar,
@@ -74,23 +87,31 @@ class CTranslater:
         self.varDict[name] += value
 
 
+
+    '''Contains all the added functions'''
     def drawCircle(self, x, y, radius):
+        #TODO: add type verif for parameters x,y,radius (and range ? yes for radius)
         self.file.write("drawCircle(renderer, " + str(x) + ", " + str(y) + ", " + str(radius) + ");\n")
 
     def drawCircleFill(self, x, y, radius):
+        #TODO: add type verif for parameters x,y,radius (and range ? yes for radius)
         self.file.write("drawCircleFill(renderer, " + str(x) + ", " + str(y) + ", " + str(radius) + ");\n")
 
     def drawRect(self, x, y, width, height):
+        #TODO: add type verif for parameters x,y,width,height (and range ? probably not)
         self.file.write(
             "drawRect(renderer, " + str(x) + ", " + str(y) + ", " + str(width) + ", " + str(height) + ");\n")
 
     def drawRectFill(self, x, y, width, height):
+        #TODO: add type verif for parameters x,y,width,height (and range ? probably not)
         self.file.write(
             "drawRectFill(renderer, " + str(x) + ", " + str(y) + ", " + str(width) + ", " + str(height) + ");\n")
 
     def setColor(self, r, g, b, a):
+        #TODO: add range and type verif for parameters r,g,b,a
         self.file.write(
             "SDL_SetRenderDrawColor(renderer, " + str(r) + ", " + str(g) + ", " + str(b) + ", " + str(a) + ");\n")
+
 
     def header(self):
         self.file.write(header)
@@ -109,27 +130,21 @@ class CTranslater:
         self.file.write("returnStatement = " + str(value) + ";\n")
 
     def add_instruction(self, instructionName, *args):
-        #print(f"Instruction Name: {instructionName}")
-        #print(f"Instruction: {self.instr[instructionName]}")
-        #print(f"Arguments: {args}")
         self.main.add_instruction(self.instr[instructionName], *args)
-        #print(*args)
 
     def run(self):
         self.main()
 
-    #TODO: not sure
     def createFunc(self, functionName, args):
         if functionName in self.instr:
-            print("Function already exist")
+            print("Function already exists")
             return
         newFunction = Function(functionName, args)
         self.instr[functionName] = newFunction
 
-    #TODO: not sure
     def addInstructionToFunction(self, functionName, instr, *args):
         if (not functionName in self.instr):
-            print("Function do not exist.")
+            print("Function does not exist.")
             return
 
         if (not instr in self.instr):
@@ -141,27 +156,3 @@ class CTranslater:
     def __del__(self):
         self.file.write(footer)
         self.file.close()
-
-
-"""if __name__ == '__main__':
-    test = CTranslater("../output/test.c")
-
-
-    test.add_instruction("createVar", "b", 105)
-    test.add_instruction("setColor", 187, 40, VarCall("b"), 0)
-    test.add_instruction("drawCircle", 158, 40, VarCall("b"))
-
-
-    test.createFunc("MyTestFunction2", ["acb"])
-    test.addInstructionToFunction("MyTestFunction2", "deb", VarCall("acb"))
-
-    test.createFunc("MyTestFunction", ["a"])
-    test.addInstructionToFunction("MyTestFunction", "deb", VarCall("a"))
-    test.addInstructionToFunction("MyTestFunction", "MyTestFunction2", "salut")
-
-    test.add_instruction("MyTestFunction", 5)
-    test.add_instruction("MyTestFunction", "Test")
-    test.add_instruction("MyTestFunction", 5)
-
-    test.run()
-"""
