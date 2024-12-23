c37035f2c321ffa7fa2fa0c6e3865dec8165930d
DonatienLinossier <donatien.linossier@gmail.com>
2024-12-23 18:45:02 +0100

    added tests for compiler and deleted unused main.c

---
+++ pydpp/compiler/CTranslater/CTranslater_test/test_04/testDrawpp.py
@@ -0,0 +1,10 @@
+from pydpp.compiler.CTranslater import *
+
+test = CTranslater(".drawppTmp/tmp.c")
+
+
+#Main
+test.add_instruction("setColor", 10, 10, 250, 1)
+test.add_instruction("drawRect", 20, 20, 20, 20)
+
+test.run()
