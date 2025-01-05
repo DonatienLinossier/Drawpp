ae5d938e326563c6c7a4174ec96341e9043f6d2d
DynamicField <dynamicfield@outlook.fr>
2025-01-05 21:23:52 +0100

    Moderately better error messages

--- libs/build_sdlEncapsulation.py
+++ libs/build_sdlEncapsulation.py
@@ -3,6 +3,7 @@
 import shutil
 import subprocess
 import os
+import sys
 
 # The directory of this script.
 script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -22,9 +23,9 @@ if os.name == "posix":
     # sdl2-config is a nice heuristic to know if SDL2 is there, but it might not be precise
     # if you're using some weird distro/config... So let's not stop the script there.
     if shutil.which("sdl2-config") is None:
-        print("Warning: Couldn't find SDL2 (sdl2-config) installed.")
-        print("On a Debian/Ubuntu system, try installing it with:")
-        print("    sudo apt-get install libsdl2-dev")
+        print("Warning: Couldn't find SDL2 (sdl2-config) installed.", file=sys.stderr)
+        print("On a Debian/Ubuntu system, try installing it with:", file=sys.stderr)
+        print("    sudo apt-get install libsdl2-dev", file=sys.stderr)
 
 if os.name == "posix" and os.getenv("FORCE_CMAKE") != "1":
     # On linux: Try calling the C compiler directly, so we don't need to install CMake.
@@ -34,7 +35,7 @@ if os.name == "posix" and os.getenv("FORCE_CMAKE") != "1":
     ar = shutil.which("ar")
 
     if not cc:
-        print("C compiler not found!")
+        print("C compiler not found!", file=sys.stderr)
         exit(1)
 
     print("Building sdlEncapsulation directly using the C compiler (" + cc + ")...")
@@ -45,18 +46,19 @@ if os.name == "posix" and os.getenv("FORCE_CMAKE") != "1":
                            f"{sdl_encap_dir}/src/sdlEncapsulation.c",  # Compile the sdlEncapsulation.c file
                            f"-I{sdl_encap_dir}/include",  # Add the include directory to the search path
                            "-o", f"{build_dir}/sdlEncapsulation.o",  # Output an object file
-                           '-lSDL2', '-lm']  # Link against SDL2 and the math library
-                          )
+                           '-lSDL2', '-lm'],  # Link against SDL2 and the math library
+                          encoding="utf-8", capture_output=True)
     if res1.returncode != 0:
-        print("Failed to compile sdlEncapsulation.c")
+        print("Failed to compile sdlEncapsulation.c:\n" + res1.stderr, file=sys.stderr)
         exit(1)
 
     # Create the static library from the only object file produced.
     # (Honestly I don't even know if that's necessary)
     res2 = subprocess.run(
-        [ar, "rcs", f"{sdl_encap_dir}/bin/libsdlEncapsulation.a", f"{build_dir}/sdlEncapsulation.o"])
+        [ar, "rcs", f"{sdl_encap_dir}/bin/libsdlEncapsulation.a", f"{build_dir}/sdlEncapsulation.o"],
+        encoding="utf-8", capture_output=True)
     if res2.returncode != 0:
-        print("Failed to create static library")
+        print("Failed to archive the static library:\n" + res2.stderr, file=sys.stderr)
         exit(1)
 else:
     # On Windows/Linux forced: Use CMake to build the project.
@@ -64,10 +66,18 @@ else:
     # Find the CMake executable.
     cmake = shutil.which("cmake")
     if not cmake:
-        print("CMake is not installed!")
+        print("CMake is not installed! Install it on your system.\n"
+              "Windows:        https://cmake.org/download/\n"
+              "Debian/Ubuntu:  Run this command: sudo apt install cmake", file=sys.stderr)
         exit(1)
 
     # Initialise CMake cache in the build dir
-    subprocess.run([cmake, ".."], cwd=build_dir)
+    res3 = subprocess.run([cmake, ".."], cwd=build_dir, encoding="utf-8", capture_output=True)
+    if res3.returncode != 0:
+        print("Failed to initialise the CMake cache:\n" + res3.stderr, file=sys.stderr)
+
     # Build the project
-    subprocess.run([cmake, "--build", "."], cwd=build_dir)
+    res4 = subprocess.run([cmake, "--build", "."], cwd=build_dir, encoding="utf-8", capture_output=True)
+    if res4.returncode != 0:
+        print("Failed to build the project:\n" + res4.stderr, file=sys.stderr)
+        exit(1)
